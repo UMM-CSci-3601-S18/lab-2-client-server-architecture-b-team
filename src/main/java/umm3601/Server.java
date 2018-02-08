53a693f6713a7eeafa70fd80c4806efa380c6667
Aurora Cordes <corde171@morris.umn.edu>
2018-02-08 15:39:38 -0600

    Edit Server to have code for issue #3, listing all users.

--- src/main/java/umm3601/Server.java
+++ src/main/java/umm3601/Server.java
@@ -1,8 +1,11 @@
 package umm3601;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
+import umm3601.todo.DatabaseTodo;
+import umm3601.todo.TodoController;
 import umm3601.user.Database;
 import umm3601.user.UserController;
 
@@ -14,12 +17,15 @@ import static spark.debug.DebugScreen.*;
 public class Server {
 
   public static final String USER_DATA_FILE = "src/main/data/users.json";
+  public static final String TODO_DATA_FILE = "src/main/data/todos.json";
   private static Database userDatabase;
+  private static DatabaseTodo todoDatabase;
 
   public static void main(String[] args) {
 
     // Initialize dependencies
     UserController userController = buildUserController();
+    TodoController todoController = buildTodoController();
 
     // Configure Spark
     port(4567);
@@ -33,13 +39,16 @@ public class Server {
     // Redirects to create simpler URLs
     redirect.get("/about", "/about.html");
     redirect.get("/users", "/users.html");
-
+    redirect.get("/todos", "/todos.html");
     // API endpoints
 
     // Get specific user
     get("api/users/:id", userController::getUser);
     // List users, filtered using query parameters
     get("api/users", userController::getUsers);
+
+    // List todos, unfiltered, pure, organic, and locally grown
+    get("api/todos", todoController::getTodos);
 
     // An example of throwing an unhandled exception so you can see how the
     // Java Spark debugger displays errors like this.
@@ -81,6 +90,24 @@ public class Server {
     }
 
     return userController;
+  }
+
+  private static TodoController buildTodoController() {
+    TodoController todoController = null;
+
+    try {
+      todoDatabase = new DatabaseTodo(TODO_DATA_FILE);
+      todoController = new TodoController(todoDatabase);
+    } catch (IOException e) {
+      System.err.println("The server failed to load the user data; shutting down.");
+      e.printStackTrace(System.err);
+
+      // Shut the server down
+      stop();
+      System.exit(1);
+    }
+
+    return todoController;
   }
 
   // Enable GZIP for all responses
