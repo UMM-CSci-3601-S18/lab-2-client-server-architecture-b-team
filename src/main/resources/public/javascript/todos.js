cb3fb057e964a2909d274e8b48b1e3851fa21c54
Aurora Cordes <corde171@morris.umn.edu>
2018-02-10 11:04:12 -0600

    Change variable name in todos.js from HttpClient to HttpThing, and clean up todos.html a little bit. Fixes #3.

--- src/main/resources/public/javascript/todos.js
+++ src/main/resources/public/javascript/todos.js
@@ -1,8 +1,8 @@
 function getAllTodos() {
   console.log("Getting all the todos.");
 
-  var HttpClient = new HttpClient();
-  HttpClient.get("/api/todos", function(returned_json){
+  var HttpThing = new HttpClient();
+  HttpThing.get("/api/todos", function(returned_json){
     document.getElementById('jsonDump').innerHTML = returned_json;
   });
 }
